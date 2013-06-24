1cb7fb670667380ea38f14a697a2eecde0f7cddb
generalzhou <jackzhou999@gmail.com>
2013-06-24 16:55:41 -0500

    removed unecessary comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,18 @@
 HackerBlues::Application.routes.draw do
-  #Comments
   resources :comments
 
-  #Posts
   resources :posts
 
-  #Comment_Votes
   resources :comment_votes, :only => [:create, :update, :show, :destroy]
 
-  #Post_Votes
   resources :post_votes, :only => [:create, :update, :show, :destroy]
 
-  #Users
   resources :users
   match 'signup' => 'users#new'
 
-  #Sessions
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy', :via => :delete
 
-  #Root
   root :to => 'posts#index'
 end
