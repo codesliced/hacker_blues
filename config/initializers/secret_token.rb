d8adf0e35cd996f99f71183b0a8a9055200ba19d
generalzhou <jackzhou999@gmail.com>
2013-06-24 11:19:22 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerBlues::Application.config.secret_token = '4de220fc90fc3170d5a412b668605f74eb3d8c12fc7d7bc95129c53620c8ccf96b3cdb9874702690557e9884c3c84ac507d61cbd87c141ec3b44cee79e4229e1'
