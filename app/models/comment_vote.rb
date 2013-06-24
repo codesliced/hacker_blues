d62882789cc0e9e9781175576f4db436fce0f1c1
generalzhou <jackzhou999@gmail.com>
2013-06-24 15:39:16 -0500

    fixed indentation

--- app/models/comment_vote.rb
+++ app/models/comment_vote.rb
@@ -1,4 +1,4 @@
 class CommentVote < ActiveRecord::Base
-    belongs_to :comment
-    belongs_to :voter, class_name:'User', :foreign_key => :user_id
+  belongs_to :comment
+  belongs_to :voter, class_name:'User', :foreign_key => :user_id
 end
