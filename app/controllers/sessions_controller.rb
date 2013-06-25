97550fabc2d512a9c98846bc9a9c9b6d80014c12
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-25 08:21:48 -0500

    added links to login and logout to layout.erb, worked on sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if user = User.authenticate(params[:username], params[:password])
+      session[:current_user_id] = user.id
+      redirect_to_root_url
+    end
   end
 
   def destroy
+    reset_session
+    redirect_to_root_url
   end
 
 end
